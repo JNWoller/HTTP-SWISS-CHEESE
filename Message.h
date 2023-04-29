f718dd078dbcea8f44e424cf55b402925173c2ed
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2023-04-29 13:05:55 -0500

    Update Message.h

--- Message.h
+++ Message.h
@@ -3,10 +3,9 @@
 
 class Message {
 public:
-    // Virtual destructor
+    
     virtual ~Message() = default;
 
-    // Pure virtual function for processing requests
     virtual void labelLayer() = 0;
 };
 
