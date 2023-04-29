40d0cc6484ebc22cac824f0f7ed840655e38e037
JNWoller <126735014+JNWoller@users.noreply.github.com>
2023-04-29 13:25:25 -0500

    Update TCPToIPv4.h

--- TCPToIPv4.h
+++ TCPToIPv4.h
@@ -26,8 +26,39 @@ class IPv4Packet: public Message {
 	MessageBuffer& message_buffer_;
 	Message* next_layer_;
 
-
 public:
+
+	//setters
+	void setVersion(uint8_t x);
+	void setIHL(uint8_t x);
+	void setDSCP(uint8_t x);
+	void setENC(uint8_t x);
+	void setTotalLength(uint16_t x);
+	void setIdentification(uint16_t x);
+	void setFlags(uint8_t x);
+	void setFragmentOffset(uint16_t x);
+	void setTimeToLive(uint8_t x);
+	void setProtocol(uint8_t x);
+	void setHeaderChecksum(uint16_t x);
+	void setSourceIP(std::string x);
+	void setDestinationIP(std::string x);
+	void setOptions(uint32_t x);
+
+	//getters
+	uint8_t getVersion() const;
+	uint8_t getIHL() const;
+	uint8_t getDSCP() const;
+	uint8_t getENC() const;
+	uint16_t getTotalLength() const;
+	uint16_t getIdentification() const;
+	uint8_t getFlags() const;
+	uint16_t getFragmentOffset() const;
+	uint8_t getTimeToLive() const;
+	uint8_t getProtocol() const;
+	uint16_t getHeaderChecksum() const;
+	std::string getSourceIP();
+	std::string getDestinationIP();
+	uint32_t getOptions() const;
 
 	//constructer
 	IPv4Packet(MessageBuffer& message_buffer, Message* next_layer);
