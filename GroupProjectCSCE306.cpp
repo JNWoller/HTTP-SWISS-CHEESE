42471800c894cfe1e94110516667daf2c5ed5782
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2023-04-29 15:43:38 -0500

    Update GroupProjectCSCE306.cpp

--- GroupProjectCSCE306.cpp
+++ GroupProjectCSCE306.cpp
@@ -1,9 +1,9 @@
 #include "ApplicationLayer.h"
 #include "TCPLayer.h"
+#include "TCPToIPv4.h"
 #include "MessageBuffer.h"
 #include "ethernetToWifi.h"
 #include <iostream>
-using namespace std;
 
 int main() {
 
@@ -18,27 +18,28 @@ int main() {
     TCPLayer segment(mes_buffer, &app_layer);
 
     segment.generate_tcp_segment();
-	
+
     IPv4Packet packet(mes_buffer, &segment);
 
     packet.packetGenerator();
 
-	wifiPacket wifiPacket;
-    
-	if (buff.is_empty() == false)
-	{
-		wifiPacket.setIPv4Packet(dynamic_cast<IPv4Packet*>(buff.dequeue()));
-	}
-	if (buff.is_empty() == true)
-	{
-		wifiPacket.enqueue(&test2);
-	}
+    wifiPacket wifiPacket;
 
-	wifiPacket.getMac1();
-	wifiPacket.getMac2();
-	wifiPacket.getMac3();
-	wifiPacket.getMac4();
-    
-    
+    if (mes_buffer.is_empty() == false)
+    {
+        wifiPacket.setIPv4Packet(dynamic_cast<IPv4Packet*>(mes_buffer.dequeue()));
+    }
+    if (mes_buffer.is_empty() == true)
+    {
+        mes_buffer.enqueue(&wifiPacket);
+    }
+    wifiPacket.getMac1();
+
+    wifiPacket.getMac2();
+
+    wifiPacket.getMac3();
+
+    wifiPacket.getMac4();
+
     return 0;
 }
