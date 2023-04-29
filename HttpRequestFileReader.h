c2451d28aeb7c0b4d33edcb113d36cc86dc4da65
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2023-04-29 13:07:38 -0500

    Update HttpRequestFileReader.h

--- HttpRequestFileReader.h
+++ HttpRequestFileReader.h
@@ -5,15 +5,21 @@
 #include <map>
 
 class HttpRequestFileReader {
+    
 public:
+    
     void readRequest(const std::string& filename);
+    
     void set_value(const std::string& key, const std::string& value);
+    
     std::string get_value(const std::string& key) const;
 
 private:
+    
     void processLine(const std::string& line);
+    
     std::map<std::string, std::string> request_data_;
 };
 
-#endif // HTTP_REQUEST_READER_H
+#endif 
 
