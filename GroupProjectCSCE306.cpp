97ccc701bdb6c18564426030f93937d609b96f52
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2023-04-30 17:36:22 -0500

    Update GroupProjectCSCE306.cpp

--- GroupProjectCSCE306.cpp
+++ GroupProjectCSCE306.cpp
@@ -1,61 +1,22 @@
-#include "ApplicationLayer.h"
-#include "TCPLayer.h"
-#include "TCPToIPv4.h"
-#include "MessageBuffer.h"
-#include "ethernetToWifi.h"
-#include "Serializer.h"
-#include <fstream>
+#include "NetworkSimulation.h"
 #include <iostream>
 
 int main() {
-
-    ApplicationLayer app_layer;
-
-    app_layer.process_requests();
-
-    MessageBuffer mes_buffer;
-
-    mes_buffer.enqueue(&app_layer);
-
-    TCPLayer segment(mes_buffer, &app_layer);
-
-    segment.generate_tcp_segment();
-
-    IPv4Packet packet(mes_buffer, &segment);
-
-    packet.packetGenerator();
-
-    wifiPacket wifiPacket;
-
-    if (mes_buffer.is_empty() == false)
-    {
-        wifiPacket.setIPv4Packet(dynamic_cast<IPv4Packet*>(mes_buffer.dequeue()));
-    }
-    if (mes_buffer.is_empty() == true)
-    {
-        mes_buffer.enqueue(&wifiPacket);
-    }
-    wifiPacket.getMac1();
-
-    wifiPacket.getMac2();
-
-    wifiPacket.getMac3();
-
-    wifiPacket.getMac4();
-
-
-	serializer output;
-    
-	if (buff.is_empty() == false)
-	{
-		output.setWifiPacket(dynamic_cast<wifiPacket*>(buff.dequeue()));
-	}
-
-	ofstream outFile("data.txt");
-
-	outFile << output;
-    
-	outFile.close();
-
+    char user_choice;
+    NetworkSimulation simulation;
+    std::cout << "Welcome to the Network Simulation Program!" << std::endl;
+    do {
+        std::cout << "Press 's' to start the process or 'q' to quit: ";
+        std::cin >> user_choice;
+        if (user_choice == 's') {
+            std::cout << "" << std::endl;
+            simulation.run_process();
+            std::cout << "Process completed!" << std::endl;
+        }
+        else if (user_choice != 'q') {
+            std::cout << "Invalid input, please try again." << std::endl;
+        }
+    } while (user_choice != 'q');
+    std::cout << "Goodbye! Program has offical terminated." << std::endl;
     return 0;
 }
