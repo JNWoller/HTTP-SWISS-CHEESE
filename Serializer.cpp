6b1c777a532498c657d5e99637dd34b545424b66
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2023-04-30 17:38:35 -0500

    Update Serializer.cpp

--- Serializer.cpp
+++ Serializer.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void serializer::labelLayer() //Inheritance
 {
+    std::cout << "Physical Layer: Local text file contains serialized object in hexadecimal." << std::endl;
 }
 
 void serializer::setWifiPacket(wifiPacket* in)
@@ -12,12 +13,10 @@ void serializer::setWifiPacket(wifiPacket* in)
     packet = in;
 }
 
-
-// Output stream operator
-
-
-ostream& operator<<(ostream& outsteam, const serializer& obj) 
+ostream& operator<<(ostream& outsteam, const serializer& obj)
 {
-    outsteam << obj.packet;
+    if (obj.packet) {
+        outsteam << obj.packet;
+    }
     return outsteam;
 }
