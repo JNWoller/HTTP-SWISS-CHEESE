8ef813f19f7d83329c426f2cbb39f53db32aa989
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2023-04-30 17:37:53 -0500

    Update MessageBuffer.cpp

--- MessageBuffer.cpp
+++ MessageBuffer.cpp
@@ -2,28 +2,40 @@
 #include <iostream>
 
 void MessageBuffer::enqueue(Message* message) {
-    
+
     buffer.push(message);
-    
-    std::cout << "Message pushed" << std::endl;
+
+    std::cout << "" << std::endl;
+
+    std::cout << "*Message pushed into buffer*" << std::endl;
+
+    std::cout << "" << std::endl;
 }
+
 Message* MessageBuffer::dequeue() {
-    
+
     if (is_empty()) {
-        
-        std::cout << "Nothing in buffer" << std::endl;
-        
+
+        std::cout << "*Nothing in buffer*" << std::endl;
+
+        std::cout << "" << std::endl;
+
         return nullptr;
     }
     Message* message = buffer.front();
     
     buffer.pop();
-    
-    std::cout << "Message popped" << std::endl;
+
+    std::cout << "" << std::endl;
+
+    std::cout << "*Message popped from the buffer*" << std::endl;
+
+    std::cout << "" << std::endl;
     
     return message;
 }
+
 bool MessageBuffer::is_empty() const {
-    
+
     return buffer.empty();
 }
