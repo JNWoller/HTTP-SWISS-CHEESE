3c5da6e4728e3dee8976ad6a772ab6ddd0327a38
LaneBrinkman75 <126734799+LaneBrinkman75@users.noreply.github.com>
2023-04-30 18:06:06 -0500

    Update ethernetToWifi.h

--- ethernetToWifi.h
+++ ethernetToWifi.h
@@ -8,29 +8,49 @@ class wifiPacket : public Message
 {
 public:
 	wifiPacket();
+	
 	void labelLayer();
+	
 	IPv4Packet* getPacket();
+	
 	string getMac1();
+	
 	string getMac2();
+	
 	string getMac3();
+	
 	string getMac4();
+	
 	void setIPv4Packet(IPv4Packet* in);
+	
 	void setMac1();
+	
 	void setMac2();
+	
 	void setMac3();
+	
 	void setMac4();
 
 private:
 	IPv4Packet* packet;
+	
 	uint8_t frameControl;
+	
 	uint8_t duration;
+	
 	string mac1_destination;
+	
 	string mac2_scource;
+	
 	string mac3_router;
+	
 	uint8_t seq_control;
+	
 	string mac4_ap;
+	
 	uint8_t crc;
+	
 	string randomMACGenerator();
 };
-#endif //ethernetToWifi_H
+#endif 
 #pragma once
